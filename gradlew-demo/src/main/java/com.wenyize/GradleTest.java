bdea4d3f1b4cc4bcc1d5b462a0dae35a290435fe
wenyz <627449989@qq.com>
2019-05-08 00:21:05 +0800

    update

---
+++ gradlew-demo/src/main/java/com.wenyize/GradleTest.java
@@ -0,0 +1,8 @@
+package com.wenyize;
+
+public class GradleTest{
+
+    public static void main(String[] args) {
+        System.out.println("hello gradle w emo");
+    }
+}
---
+++ pythrift/py_client.py
@@ -0,0 +1,40 @@
+
+
+__author__ = '作者'
+
+
+from py.thrift.generated import PersonService
+from py.thrift.generated import ttypes
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TCompactProtocol
+try:
+    tSocket = TSocket.TSocket('localhost',8899)
+    tSocket.setTimeout(600)
+
+    transport = TTransport.TFramedTransport(tSocket)
+    protocol = TCompactProtocol.TCompactProtocol(transport)
+    client = PersonService.Client(protocol)
+
+    transport.open()
+    person = client.getPersonByUserName("张三")
+
+    print(person.username)
+    print(person.age)
+    print(person.married)
+
+    newPerson = ttypes.Person()
+    newPerson.username = "李四"
+    newPerson.age = 25
+    newPerson.married = True
+
+    client.savePerson(newPerson)
+
+    transport.close()
+
+
+
+except Thrift.TException as tx:
+    print("%s",tx.message)
